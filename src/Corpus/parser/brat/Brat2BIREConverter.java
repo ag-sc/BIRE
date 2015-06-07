bbd64e4c57fe21eb42a4e71bd788c3947a95af3c
sjebbara <s.jebbara@gmail.com>
2015-06-07 22:26:01 +0200

    Conversion from Brat to BIRE format adapted to new "Weak Reference" scheme. Still, tokens need to be extracted and the begin and end indices of entities need to be initialized correctly.

--- src/Corpus/parser/brat/Brat2BIREConverter.java
+++ src/Corpus/parser/brat/Brat2BIREConverter.java
@@ -45,116 +45,51 @@ public class Brat2BIREConverter {
 		return manager;
 	}
 
-	/**
-	 * Converts the Brat-TextBasedAnnotation t into an EntityAnnotation and adds
-	 * it to a global Map.
-	 * 
-	 * @param manager
-	 * 
-	 * @param config
-	 * 
-	 * @param t
-	 */
 	private void convertTextBoundAnnotation(EntityManager manager,
 			AnnotationConfig config, BratTextBoundAnnotation t) {
-		EntityAnnotation entity = getOrCreateEntityByID(t.getID());
+		EntityAnnotation entity = new EntityAnnotation(manager, t.getID());
 		EntityType entityType = config.getEntityType(t.getRole());
 		entity.init(entityType, t.getStart(), t.getEnd(), t.getText());
 		// entities.put(t.getID(), entity);
+		manager.addEntityAnnotation(entity);
 	}
 
-	/**
-	 * Converts the Brat-EventAnnotation into a several RelationAnnotations. By
-	 * combining the trigger of the event with each single argument, the n-ary
-	 * event is split into n binary relations. If an argument is an event on its
-	 * own, its trigger is used as part of the relation.
-	 * 
-	 * @param manager
-	 * 
-	 * @param config
-	 * 
-	 * @param e
-	 */
 	private void convertEventAnnotation(EntityManager manager,
 			AnnotationConfig config, BratEventAnnotation e) {
-		Map<String, EntityAnnotation> arguments = new HashMap<String, EntityAnnotation>();
+		Map<String, String> arguments = new HashMap<String, String>();
 		for (Entry<String, BratAnnotation> entry : e.getArguments().entrySet()) {
 			BratAnnotation ann = entry.getValue();
-			EntityAnnotation arg = resolveReference(ann);
-			arguments.put(entry.getKey(), arg);
+			// Entities in the BIRE annotation implementation only keep weak
+			// references (IDs) to other entities.
+			arguments.put(entry.getKey(), ann.getID());
 		}
-		EntityAnnotation entity = getOrCreateEntityByID(e.getID());
+		EntityAnnotation entity = new EntityAnnotation(manager, e.getID());
 		EntityType entityType = config.getEntityType(e.getRole());
 		entity.init(entityType, arguments, e.getTrigger().getStart(), e
 				.getTrigger().getEnd(), e.getTrigger().getText());
+		manager.addEntityAnnotation(entity);
 	}
 
 	private void convertRelationAnnotation(EntityManager manager,
 			AnnotationConfig config, BratRelationAnnotation t) {
-		Map<String, EntityAnnotation> arguments = new HashMap<String, EntityAnnotation>();
+		Map<String, String> arguments = new HashMap<String, String>();
 		for (Entry<String, BratAnnotation> entry : t.getArguments().entrySet()) {
 			BratAnnotation ann = entry.getValue();
-			EntityAnnotation arg = resolveReference(ann);
-			arguments.put(entry.getKey(), arg);
+			arguments.put(entry.getKey(), ann.getID());
 		}
-		EntityAnnotation entity = getOrCreateEntityByID(t.getID());
+		EntityAnnotation entity = new EntityAnnotation(manager, t.getID());
 		EntityType entityType = config.getEntityType(t.getRole());
 		/*
 		 * TODO relations are not motivated by tokens in the text and thus have
 		 * no start, end and text attribute. Here, these values are filled with
 		 * placeholder values
 		 */
-		entity.init(entityType, -1, -1, "");
+		entity.init(entityType, arguments, -1, -1, "");
+		manager.addEntityAnnotation(entity);
 	}
 
 	private void convertAttributeAnnotation(EntityManager manager,
 			AnnotationConfig config, BratAttributeAnnotation t) {
 		// TODO attributes are currently neglected
-	}
-
-	/**
-	 * Retrieves an EntityAnnotation Object (or creates it if necessary) using
-	 * the given BratAnnotation. If the BratAnnotation is a
-	 * BratTextBoundAnnotation, the created EntityAnnotation resembles a direct
-	 * conversion. If it is an BratEventAnnotation, the created Entity refers to
-	 * the trigger of the event (which is in turn a BratTextBoundAnnotation).
-	 * 
-	 * @param ann
-	 * @return
-	 */
-	private EntityAnnotation resolveReference(BratAnnotation ann) {
-		EntityAnnotation entity = null;
-		if (ann instanceof BratTextBoundAnnotation) {
-			/*
-			 * Create an entity reference to a BratTextBasedAnnotation.
-			 */
-			entity = getOrCreateEntityByID(ann.getID());
-		} else if (ann instanceof BratEventAnnotation) {
-			/*
-			 * Create an entity reference to a the Trigger of an
-			 * BratEventAnnotation.
-			 */
-			BratEventAnnotation eventArg = (BratEventAnnotation) ann;
-			entity = getOrCreateEntityByID(eventArg.getTrigger().getID());
-		}
-		return entity;
-	}
-
-	/**
-	 * This methods is used to handle/resolve references to EntityAnnotations.
-	 * If no Entity with the given ID is processed yet, a corresponding
-	 * EntityAnnotations is created and returned. As soon as it is processed,
-	 * this Entity should be filled with data via the init(...) method
-	 * 
-	 * @param id
-	 * @return
-	 */
-	private EntityAnnotation getOrCreateEntityByID(String id) {
-		EntityAnnotation e = entities.get(id);
-		if (e == null) {
-			e = new EntityAnnotation(id);
-			entities.put(id, e);
-		}
-		return e;
 	}
 }
