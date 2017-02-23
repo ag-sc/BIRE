76c13d85e5d2f8eb6b181634717e477deccd239e
sjebbara <sjebbara@uni-bielefeld.de>
2017-02-23 10:12:12 +0100

    Introduced BeamSearchSampler
    BeamSearchSampler keeps K current states at each step and samples
    successor states from each of them.
    Adapted Learner/Trainer/Sampling strategies to fit needs of
    BeamSearchSampler

---
+++ src/learning/AlternatingSamplingStrategyCallback.java
@@ -0,0 +1,29 @@
+package learning;
+
+import learning.Trainer.EpochCallback;
+import sampling.BeamSearchSampler;
+import sampling.samplingstrategies.BeamSearchSamplingStrategies;
+import variables.AbstractState;
+
+public class AlternatingSamplingStrategyCallback<StateT extends AbstractState<?>> implements EpochCallback {
+	private BeamSearchSampler<?, StateT, ?> sampler;
+	private int beamSize = 10;
+
+	public AlternatingSamplingStrategyCallback(BeamSearchSampler<?, StateT, ?> sampler, int beamSize) {
+		super();
+		this.sampler = sampler;
+		this.beamSize = beamSize;
+	}
+
+	@Override
+	public void onStartEpoch(Trainer caller, int epoch, int numberOfEpochs, int numberOfInstances) {
+		if (epoch % 2 == 0) {
+			sampler.setTrainSamplingStrategy(BeamSearchSamplingStrategies.greedyBeamSearchSamplingStrategy(beamSize,
+					s -> s.getObjectiveScore()));
+		} else {
+			sampler.setTrainSamplingStrategy(
+					BeamSearchSamplingStrategies.greedyBeamSearchSamplingStrategy(beamSize, s -> s.getModelScore()));
+		}
+	}
+
+}
---
+++ src/sampling/stoppingcriterion/BeamSearchStoppingCriterion.java
@@ -0,0 +1,26 @@
+package sampling.stoppingcriterion;
+
+import java.util.List;
+
+import variables.AbstractState;
+
+/**
+ * This interface provides a way to define, when the DefaultSampler should stop
+ * sampling, e.g. after reaching a fixed amount of sampling steps, or after the
+ * model score converges.
+ * 
+ * @author sjebbara
+ *
+ * @param <StateT>
+ */
+public interface BeamSearchStoppingCriterion<StateT extends AbstractState<?>> {
+	/**
+	 * If true, the sampler will stop sampling and return the sampling chain
+	 * until that point. If false, the sampling proceeds.
+	 * 
+	 * @param chain
+	 * @param step
+	 * @return
+	 */
+	public boolean checkCondition(List<List<StateT>> chain, int step);
+}
